448017a8ad0d14713cd6f83a34f7dc20f329c289
mei <whitecard86@gmail.com>
2017-09-15 16:36:08 +0700

    exam-front-mei

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import { MyDataService } from "./my-data.service";
 
 @Component({
   selector: 'app-root',
@@ -9,37 +6,12 @@ import { MyDataService } from "./my-data.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name : string = "Name";
-  email : string = "Email";
-  number : string = "PhoneNumber";
-  picture : string ="";
-  UserList : object[] = [] ;
+  ChangeTab(UserList, AddUser) {
+    document.getElementById(UserList).classList.add('active')
+    document.getElementById(AddUser).classList.remove('active');
 
-  constructor(private http: Http, private data: MyDataService) {
+    var e = (document.getElementById("abc")) as HTMLSelectElement;
+    var value = e.options[e.selectedIndex].text;
+
   }
-
-  PostData(){
-
-  var body = JSON.stringify({
-    "email" : "sydney@fife"
-});
-  var hd = new Headers ({"Content-Type" : "application/json"});
-  var options = new RequestOptions({ headers : hd });
-
-  this.http.post("https://reqres.in/api/register", body, options)
-  .subscribe(
-    result =>{
-      console.log(result.json())
-    },
-    error=>{
-      console.log(error)
-    }
-  );
-};
-RefreshData() :void{
-return this.data.GetAlluser();
- }
-
 }
-
-
