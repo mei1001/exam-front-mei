45db35f0e7b2f88c8232f5974463a82e7b3e5168
mei <whitecard86@gmail.com>
2017-09-15 12:22:42 +0700

    exam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+
+import { MyDataService } from "./mydataservice.service";
 
 @NgModule({
   declarations: [
